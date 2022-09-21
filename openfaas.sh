0b6c1c2d6183009e5d89ede3ee4bd5c1be9893a8
fgtubs <87946264+fgtubs@users.noreply.github.com>
2022-09-21 18:01:50 +0200

    Update openfaas.sh

--- openfaas.sh
+++ openfaas.sh
@@ -1,8 +1,28 @@
-# Install openfaas with arkade
+# Install openfaas with helm
+# used documentation: https://github.com/openfaas/faas-netes/blob/master/chart/openfaas/README.md
 
-    # Install arkade
-curl -sLS https://get.arkade.dev | sudo sh
-    # Install openfaas
-arkade install openfaas
+# Install helm
+curl -sSLf https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
-sudo reboot
+# Create namespaces
+kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml
+
+# Add openfaas helm chart
+helm repo add openfaas https://openfaas.github.io/faas-netes/
+
+helm repo update \
+ && helm upgrade openfaas --install openfaas/openfaas \
+    --namespace openfaas  \
+    --set functionNamespace=openfaas-fn \
+    --set generateBasicAuth=true \
+    --set gateway.nodePort=30080
+    
+# Receive credantials
+PASSWORD=$(kubectl -n openfaas get secret basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode) && \
+echo "OpenFaaS admin password: $PASSWORD"
+
+# Install openfaas cli
+curl -sSL https://cli.openfaas.com | sudo -E sh
+
+# Login with the CLI and check connectivity
+echo -n $PASSWORD | faas-cli login -g $OPENFAAS_URL -u admin --password-stdin
