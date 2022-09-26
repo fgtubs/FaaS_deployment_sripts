cb42917ab7576d3ce32952b6696c2d39a2dfebe4
fgtubs <87946264+fgtubs@users.noreply.github.com>
2022-09-26 16:33:14 +0200

    Update openfaas.sh

--- openfaas.sh
+++ openfaas.sh
@@ -29,5 +29,6 @@ curl -sSL https://cli.openfaas.com | sudo -E sh
 
 export OPENFAAS_URL=http://127.0.0.1:30080
 
+sleep 3
 # Login with the CLI and check connectivity
 echo -n $PASSWORD | faas-cli login -g $OPENFAAS_URL -u admin --password-stdin
