36fd54cb1d19d51f17a40182874d4a3c87de8617
fgtubs <87946264+fgtubs@users.noreply.github.com>
2022-10-06 23:13:33 +0200

    Create OpenWhisk.sh

---
+++ OpenWhisk.sh
@@ -0,0 +1,8 @@
+# Installation of OpenWhisk 
+
+# get wsk, cli
+wget https://github.com/apache/openwhisk-cli/releases/download/1.2.0/OpenWhisk_CLI-1.2.0-linux-arm64.tgz
+sudo tar -xzf OpenWhisk_CLI-1.2.0-linux-arm64.tgz
+
+mv $pwd/wsk /usr/local/bin/wsk
+
