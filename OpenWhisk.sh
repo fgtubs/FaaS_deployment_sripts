86f5601db04550016e2ed5c811cfdafd16011391
fgtubs <87946264+fgtubs@users.noreply.github.com>
2022-10-07 11:44:11 +0200

    Update OpenWhisk.sh

--- OpenWhisk.sh
+++ OpenWhisk.sh
@@ -4,5 +4,5 @@
 wget https://github.com/apache/openwhisk-cli/releases/download/1.2.0/OpenWhisk_CLI-1.2.0-linux-arm64.tgz
 sudo tar -xzf OpenWhisk_CLI-1.2.0-linux-arm64.tgz
 
-mv $pwd/wsk /usr/local/bin/wsk
+sudo mv wsk /usr/local/bin/wsk
 
