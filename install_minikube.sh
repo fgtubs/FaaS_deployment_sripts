958c2cbee4021fcf195424def41f11e70cdc0ae0
fgtubs <87946264+fgtubs@users.noreply.github.com>
2022-10-27 10:20:11 +0200

    Update install_minikube.sh

--- install_minikube.sh
+++ install_minikube.sh
@@ -1,19 +1,11 @@
-# insatll kvm/libvirt as a hypervisor
+# First install docker with the script
 
 sudo apt update
-sudo apt install -y linux-modules-extra-raspi
-sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
-sudo adduser $USER kvm
-
-
 
 # install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-arm64
 sudo install minikube-linux-arm64 /usr/local/bin/minikube
 
-# make it easier to access
-alias kubectl="minikube kubectl --"
+minikube start
 
-sudo reboot
-
-# run: minikube start --driver=kvm2
+# to access cluster: minikube kubectl -- get po -A
