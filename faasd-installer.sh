335a6b44be3efaf7bd50fd51ca47642c2525b351
fgtubs <87946264+fgtubs@users.noreply.github.com>
2022-11-14 10:16:19 +0100

    Update faasd-installer.sh

--- faasd-installer.sh
+++ faasd-installer.sh
@@ -3,3 +3,11 @@
 git clone https://github.com/openfaas/faasd --depth=1
 cd faasd
 ./hack/install.sh
+
+# to log in: sudo cat /var/lib/faasd/secrets/basic-auth-password | faas-cli login -s
+
+# to check store functions: faas-cli store list
+
+# to deploy functions: faas-cli deploy --image fcarp10/hello-world --name hello-world // faas-cli store deploy nodeinfo
+
+# to run functions: curl http://127.0.0.1:8080/function/hello-world // curl http://127.0.0.1:8080/function/nodeinfo
